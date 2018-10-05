4bedc44aff8e8331281f68dd81cd940506394213
Kallirroi <kallirroi.retzepi@gmail.com>
2018-10-05 17:28:36 -0400

    new config

--- mosaic.py
+++ mosaic.py
@@ -4,9 +4,9 @@ from PIL import Image
 from multiprocessing import Process, Queue, cpu_count
 
 # Change these 3 config parameters to suit your needs...
-TILE_SIZE      = 50		# height/width of mosaic tiles in pixels
-TILE_MATCH_RES = 20		# tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT    = 4		# the mosaic image will be this many times wider and taller than the original
+TILE_SIZE      = 100		# height/width of mosaic tiles in pixels
+TILE_MATCH_RES = 10		# tile matching resolution (higher values give better fit but require more processing)
+ENLARGEMENT    = 2		# the mosaic image will be this many times wider and taller than the original
 
 TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
