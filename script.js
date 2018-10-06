278991bb3fd66750e1e39e2ae199a46883b7db24
Kallirroi <kallirroi.retzepi@gmail.com>
2018-10-05 22:49:43 -0400

    added form and using modules

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
+import {$} from 'https://pauls-uikit.hashbase.io/js/dom.js'
+import {renderCanvas} from './canvas.js'
 
-var canvas = d3.select("canvas").call(d3.zoom().scaleExtent([1, 10]).on("zoom", zoom)),
-    context = canvas.node().getContext("2d"),
-    width = canvas.property("width"),
-    height = canvas.property("height");
+// var pathToImage = "exported.png";
+var pathToImage = "output/nyc.jpeg";
+renderCanvas(pathToImage);
 
-var imageObj = new Image();
-imageObj.onload = function(){
-    context.drawImage(imageObj,0,0,imageObj.width,imageObj.height,0,0,width,height);
-};
-imageObj.src = "exported.png";
-
-function zoom() {
-  var transform = d3.event.transform;
-  context.save();
-  context.clearRect(0, 0, width, height);
-  context.translate(transform.x, transform.y);
-  context.scale(transform.k, transform.k);
-  draw();
-  context.restore();
-}
-
-function draw() {
-  canvas.node().getContext("2d");
-  context.drawImage(imageObj,0,0,imageObj.width,imageObj.height,0,0,width,height);
-}
+$('#add-photo').addEventListener('change', function() {
+  console.log('change!')
+});
