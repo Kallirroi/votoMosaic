c46f6b2ea7560322dcf279465a3ecbce4c4b718e
Kallirroi <kallirroi.retzepi@gmail.com>
2018-10-07 15:39:50 -0400

    no google sheet integration, trying netlify forms

--- script.js
+++ script.js
@@ -10,25 +10,3 @@ $('#add-photo').addEventListener('change', function(e) {
   e.preventDefault();
   console.log('change!');
 });
-
-const form = document.forms['submit-to-google-sheet'],
-    scriptURL = 'https://script.google.com/macros/s/AKfycbzMi5e-JvqNAQc78M06Iw1E2Gy1jzGl4A7OQXfRZuxpa01Vmxxa/exec'
-
-// form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     fetch(scriptURL, {
-//         method: 'POST',
-//         mode: "cors",
-//         crossDomain: true,
-//         body: new FormData(form),
-//         headers: {
-//             "Content-Type": "application/json; charset=utf-8",
-//             'Access-Control-Allow-Headers': '*',
-//             'Access-Control-Allow-Credentials': true,
-//             'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-//             "Access-Control-Allow-Origin" : "*"
-//         }
-//       })
-//       .then(response => console.log('Success!', response))
-//       .catch(error => console.error('Error!', error.message))
-// })
